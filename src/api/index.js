fb36552822ae2769d3ee8bc21617b89fdf83cbba
Dmitrii Isakov <dimass82@mail.ru>
2021-09-05 08:56:57 +0300

    pagination & proptypes

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import * as users from "./fake.api/user.api"
-const api = {
-    users
-}
-export default api
---
+++ src/app/components/users.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import User from './user';
+import Pagination from './pagination';
+import { paginate } from '../utils/paginate';
+import PropTypes from 'prop-types';
+
+const Users = ({ users: allUsers, ...rest }) => {
+    const count = allUsers.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+    const users = paginate(allUsers, currentPage, pageSize);
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+Users.propTypes = {
+    allUsers: PropTypes.array,
+    users: PropTypes.array.isRequired
+};
+
+export default Users;
